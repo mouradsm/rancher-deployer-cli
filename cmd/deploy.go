c8a2e2a78c3b492a461fd3daf70f1ae17c746759
Diego Moura <diego.moura@theideaofnow.com>
2023-06-22 23:23:32 -0300

    update command with some flags

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -1,40 +1,59 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // deployCmd represents the deploy command
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Deploy a workload to rancher",
+	Long:  `Deploy, creating or updating, a workload to a k8s cluster managed by rancher`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deploy called")
+		//fmt.Println("deploy called")
 	},
 }
+
+//flags
+
+var RancherUrl string
+var RancherKey string
+var RancherSecret string
+var Cluster string
+var Project string
+var Namespace string
+var Deployment string
+var Image string
+var VerifySSL bool
+var ServiceName string
+var ServiceListeningPort string
+var ServiceTargetPort string
+var ServiceSelectorLabel string
+var ServiceSelectorValue string
 
 func init() {
 	rootCmd.AddCommand(deployCmd)
 
-	// Here you will define your flags and configuration settings.
+	deployCmd.Flags().StringVarP(&RancherUrl, "rancher-url", "u", "", "rancher server url (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "rancher-key", "k", "", "rancher server key (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "rancher-secret", "s", "", "rancher server secret (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "cluster", "c", "", "rancher cluster name (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "project", "p", "", "rancher project name (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "namespace", "n", "default", "kubernetes namespace name")
+	deployCmd.Flags().StringVarP(&RancherUrl, "deployment", "d", "", "kubernetes deployment name (required)")
+	deployCmd.Flags().StringVarP(&RancherUrl, "image", "i", "", "docker image (required)")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
+	//deployCmd.Flags().BoolVar(&VerifySSL, "no-ssl-verify", true, "Flag to disable ssl verify on self-signed certs")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	deployCmd.MarkFlagRequired("rancher-url")
+	deployCmd.MarkFlagRequired("rancher-key")
+	deployCmd.MarkFlagRequired("rancher-secret")
+	deployCmd.MarkFlagRequired("cluster")
+	deployCmd.MarkFlagRequired("project")
+	//deployCmd.MarkFlagRequired("namespace")
+	deployCmd.MarkFlagRequired("deployment")
+	deployCmd.MarkFlagRequired("image")
 }
